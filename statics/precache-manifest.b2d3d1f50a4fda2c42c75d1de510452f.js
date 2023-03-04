85fdba29bd1e9c38c8c18c332bea551a8d9c10a2
VirtualHotBar <96966978+VirtualHotBar@users.noreply.github.com>
2023-03-04 11:59:04 +0800

    Add files via upload

---
+++ statics/precache-manifest.b2d3d1f50a4fda2c42c75d1de510452f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "869e27e7b01b958f7bb1a7ddf24419aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6a35068dfbd4082398f",
+    "url": "/static/css/6.dac0b951.chunk.css"
+  },
+  {
+    "revision": "00a66adbbb5fdb16aa01",
+    "url": "/static/js/10.8e0e1591.chunk.js"
+  },
+  {
+    "revision": "d084d0ada7e27df3ad95",
+    "url": "/static/js/11.ab19f151.chunk.js"
+  },
+  {
+    "revision": "738069fbdea212f67b41",
+    "url": "/static/js/12.93b2d9c6.chunk.js"
+  },
+  {
+    "revision": "7326878d81882ed049b1",
+    "url": "/static/js/13.df72355a.chunk.js"
+  },
+  {
+    "revision": "c6a35068dfbd4082398f",
+    "url": "/static/js/6.465d33b6.chunk.js"
+  },
+  {
+    "revision": "b4ddcbe0ecad0c8fcc04",
+    "url": "/static/js/7.f919f3de.chunk.js"
+  },
+  {
+    "revision": "bcd0c17e1378bb937a10",
+    "url": "/static/js/8.3771fbfa.chunk.js"
+  },
+  {
+    "revision": "88cf912e1b3b11a350a6",
+    "url": "/static/js/9.8aae53eb.chunk.js"
+  },
+  {
+    "revision": "1b6bdb317c3595c506d6",
+    "url": "/static/js/codeEditor.714f9d2f.chunk.js"
+  },
+  {
+    "revision": "3fa91e4628f42d3894e9",
+    "url": "/static/js/main.3d77f074.chunk.js"
+  },
+  {
+    "revision": "72cdeb6339b4b110eef6",
+    "url": "/static/js/runtime-main.361d2193.js"
+  },
+  {
+    "revision": "51c934afb61ee321ae0d9f05a4b432e9",
+    "url": "/static/media/getFetch.51c934af.cjs"
+  }
+]);
